72f262fcf170a4a1ca0df9aa43a1da6466d0bbe9
dio067 <yousefnas06@gmail.com>
2026-02-16 23:09:20 +0400

    feat: provide the google oAuth credentials as an arguments of GoogleStrategy

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,22 @@ import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    },
+  ),
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
