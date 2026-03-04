2a164163fbd3ce55a716075369c21b69384837e3
dio067 <yousefnas06@gmail.com>
2026-03-04 14:02:05 +0400

    fix(syntax): correct the spelling of DEFAULT property

--- server/src/schema/users.ts
+++ server/src/schema/users.ts
@@ -7,7 +7,7 @@ const createUsersTable = async () => {
          googleId VARCHAR(255) UNIQUE NOT NULL,
          name VARCHAR(255) NOT NULL,
          email VARCHAR(255) UNIQUE NOT NULL,
-         created_at TIMESTAMP DFAULT NOW()
+         created_at TIMESTAMP DEFAULT NOW()
         )`);
 };
 
