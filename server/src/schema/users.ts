659adc9aa97291b3b90119fb230cc59969fe9883
dio067 <yousefnas06@gmail.com>
2026-03-04 13:59:06 +0400

    feat: add googlId column inside users schema

--- server/src/schema/users.ts
+++ server/src/schema/users.ts
@@ -4,6 +4,7 @@ const createUsersTable = async () => {
   await pool.query(`
         CREATE TABLE IF NOT EXISTS users(
          id SERIAL PRIMARY KEY,
+         googleId VARCHAR(255) UNIQUE NOT NULL,
          name VARCHAR(255) NOT NULL,
          email VARCHAR(255) UNIQUE NOT NULL,
          created_at TIMESTAMP DFAULT NOW()
