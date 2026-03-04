4b1e730d4001f752200cf54eba75bdc72422dd93
dio067 <yousefnas06@gmail.com>
2026-03-04 15:08:35 +0400

    refactor change column naming format from camel to snake case

--- server/src/schema/users.ts
+++ server/src/schema/users.ts
@@ -4,7 +4,7 @@ const createUsersTable = async () => {
   await pool.query(`
         CREATE TABLE IF NOT EXISTS users(
          id SERIAL PRIMARY KEY,
-         googleId VARCHAR(255) UNIQUE NOT NULL,
+         google_id VARCHAR(255) UNIQUE NOT NULL,
          name VARCHAR(255) NOT NULL,
          email VARCHAR(255) UNIQUE NOT NULL,
          created_at TIMESTAMP DEFAULT NOW()
