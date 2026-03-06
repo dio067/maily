889bf0ccecdb03f1023350587310e1d9934bbf9b
dio067 <yousefnas06@gmail.com>
2026-03-06 23:05:28 +0400

    feat: add deseralize function to insert session to each request

--- server/src/services/passportGoogle.ts
+++ server/src/services/passportGoogle.ts
@@ -1,4 +1,5 @@
 import passport, { Profile } from 'passport';
+import pool from '../config/db';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { findOrCreateNewUser } from '../models/user.model';
 
@@ -9,8 +10,13 @@ dotenv.config();
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '';
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || '';
 
-passport.serializeUser((user: any, done) => {
+passport.serializeUser((user: any, done: Function) => {
   done(null, user.id);
+});
+
+passport.deserializeUser(async (id: number, done: Function) => {
+  const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [id]);
+  done(null, result.rows[0]);
 });
 
 passport.use(
