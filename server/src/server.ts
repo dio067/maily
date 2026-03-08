f4f7b3fface81c94ad1504c97fde84d0317c982f
dio067 <yousefnas06@gmail.com>
2026-03-09 00:28:25 +0400

    feat: enable cookie session

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieSession from 'cookie-session';
+import passport from 'passport';
 import './services/passportGoogle';
 import authRouter from './routes/authRoutes';
 import initDB from './schema';
 
 const PORT = process.env.PORT || 5000;
+const COOKIE_KEY = process.env.COOKIE_KY || '';
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000,
+    keys: [COOKIE_KEY],
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(authRouter);
 
