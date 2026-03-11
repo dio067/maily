7c954b71cfddc93ee8b4b941cf06266f0ea82afe
dio067 <yousefnas06@gmail.com>
2026-03-11 20:37:03 +0400

    chore: add redux store setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 import App from "./components/App";
 
 const el = document.querySelector("#root")!;
+
+const store = createStore(() => {}, applyMiddleware());
 
 const root = createRoot(el);
 
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      {" "}
+      <App />
+    </Provider>
   </StrictMode>,
 );
