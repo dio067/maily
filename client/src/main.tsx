1d05fec4e94a77c773815e3516c5494acd840629
dio067 <yousefnas06@gmail.com>
2026-03-11 19:42:26 +0400

    refactor: restructure the file and exclude non-needed import statements

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
 
-createRoot(document.getElementById('root')!).render(
+const el = document.querySelector("#root")!;
+
+const root = createRoot(el);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
