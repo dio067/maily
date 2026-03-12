881a8c08b50fc904abfcdccc157181d36898e36f
dio067 <yousefnas06@gmail.com>
2026-03-13 03:31:16 +0400

    feat: structure the header component with basic styles

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,3 +1,10 @@
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <nav className='flex sticky top-0 z-index mb-7 justify-between border-b border-white shadow-xl bg-gray-300 h-full'>
+      <h1 className='py-4 px-6 m-2 font-bold text-2xl'>Maily</h1>
+      <button className=' m-4 w-21 h-12 border cursor-pointer  hover:-translate-y-0.5 hover:shadow-2xl  transition-all bg-black text-white rounded-2xl font-bold'>
+        Login
+      </button>
+    </nav>
+  );
 }
