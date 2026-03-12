81b3b8656c36c735513294f08d74af1261ebba2b
dio067 <yousefnas06@gmail.com>
2026-03-12 15:31:02 +0400

    fix: remove createStore setup from the root

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createStore, applyMiddleware } from "redux";
+import store from "./store";
 import { Provider } from "react-redux";
 import App from "./components/App";
 
 const el = document.querySelector("#root")!;
-
-const store = createStore(() => {}, applyMiddleware());
-
 const root = createRoot(el);
-
 root.render(
   <StrictMode>
     <Provider store={store}>
