e4c16283495ee8e02908ac81f6c590b6a4fb1676
dio067 <yousefnas06@gmail.com>
2026-03-13 17:43:12 +0400

    feat: updated styles of header nav and buttons colors

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,11 @@
 export default function Header() {
   return (
-    <nav className='flex sticky top-0 z-index mb-7 justify-between border-b border-white shadow-xl bg-gray-300 h-full'>
-      <h1 className='py-4 px-6 m-2 font-bold text-2xl'>Maily</h1>
-      <button className=' m-4 w-21 h-12 border cursor-pointer  hover:-translate-y-0.5 hover:shadow-2xl  transition-all bg-black text-white rounded-2xl font-bold'>
-        Login
+    <nav className='flex sticky top-0 z-index mb-7 justify-between border-b border-white shadow-xl bg-orange-300 h-full'>
+      <h1 className='py-4 px-6 m-2 italic text-white  font-bold text-3xl'>
+        Maily
+      </h1>
+      <button className=' m-4 w-45 h-12 italic border-3  border-white cursor-pointer hover:w-2xs  hover:-translate-y-0.5 hover:shadow-2xl  transition-all bg-orange-300 text-white rounded-2xl font-bold'>
+        <h1 className=''>Login With Google</h1>
       </button>
     </nav>
   );
