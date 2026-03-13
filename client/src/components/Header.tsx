e641bb94cc3db6dd5fd8a6646751c6bb4b501d26
dio067 <yousefnas06@gmail.com>
2026-03-13 17:49:42 +0400

    refactor: add cursor-pointer class for the header title

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <nav className='flex sticky top-0 z-index mb-7 justify-between border-b border-white shadow-xl bg-orange-300 h-full'>
-      <h1 className='py-4 px-6 m-2 italic text-white  font-bold text-3xl'>
-        Maily
+      <h1 className='py-4 px-6 m-2 italic text-white cursor-pointer font-bold text-3xl'>
+        Maily -3{" "}
       </h1>
       <button className=' m-4 w-45 h-12 italic border-3  border-white cursor-pointer hover:w-2xs  hover:-translate-y-0.5 hover:shadow-2xl  transition-all bg-orange-300 text-white rounded-2xl font-bold'>
         <h1 className=''>Login With Google</h1>
