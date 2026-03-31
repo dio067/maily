26c61464f70c0ac6495292d86e0b81b9e04fa424
dio067 <yousefnas06@gmail.com>
2026-04-01 02:38:53 +0400

    feat: add fetchUser query and export its hook

--- client/src/store/apis/authApi.ts
+++ client/src/store/apis/authApi.ts
@@ -1,1 +1,18 @@
+import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { createApi } from "@reduxjs/toolkit/query/react";
+
+const authApi = createApi({
+  reducerPath: "auth",
+  baseQuery: fetchBaseQuery({ baseUrl: "/api/" }),
+  endpoints(builder) {
+    return {
+      fetchUser: builder.query({
+        query: () => {
+          return `current_user`;
+        },
+      }),
+    };
+  },
+});
+
+export const { useFetchUserQuery } = authApi;
