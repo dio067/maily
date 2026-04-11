2889697949c5b6b5a60320cf8461d167329e491c
dio067 <yousefnas06@gmail.com>
2026-04-11 14:11:04 +0400

    feat(store): add listener middleware to update auth state based on endpoint satisfaction

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,12 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createListenerMiddleware } from "@reduxjs/toolkit";
+import { authReducer } from "./slices/authSlice";
+import { setUser } from "./slices/authSlice";
 import { authApi } from "./apis/authApi";
+
+const listenerMiddleware = createListenerMiddleware();
+
+listenerMiddleware.startListening({
+  matcher: authApi.endpoints.fetchUser.matchFulfilled,
+  effect: (action, listenerApi) => {
+    console.log("matchFulfilled fired!", action.payload);
+
+    if (action.payload === null) {
+      listenerApi.dispatch(setUser(false));
+    } else {
+      listenerApi.dispatch(setUser(true));
+    }
+  },
+});
 
 const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(authApi.middleware);
+    return getDefaultMiddleware()
+      .concat(authApi.middleware)
+      .concat(listenerMiddleware.middleware);
   },
 });
 
